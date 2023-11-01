72ba141894531e30db16d247afd08164ac6963ce
Logan Mancuso <mickeyacejr@live.com>
2023-10-31 22:24:58 -0400

    update packer username outputs

--- outputs.tf
+++ outputs.tf
@@ -5,14 +5,18 @@
 #
 ##############################################################################
 
-output "instance_ssh_pubkey" {
-  description = "instance public ssh key"
-  value       = var.instance_ssh_pubkey
+output "instance_username" {
+  value = "instance-user"
 }
 
 output "instance_password_hashed" {
   sensitive = true
   value = var.hashed_password
+}
+
+output "instance_ssh_pubkey" {
+  description = "instance public ssh key"
+  value       = var.instance_ssh_pubkey
 }
 
 output "vm_template_id" {
